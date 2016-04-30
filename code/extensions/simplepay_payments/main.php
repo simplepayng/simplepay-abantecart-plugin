b8db19fbf66f243eec6771ebd53269eb13906c52
André Almeida <andrealmieda@gmail.com>
2016-04-30 21:06:35 +0100

    Initial release

---
+++ code/extensions/simplepay_payments/main.php
@@ -0,0 +1,29 @@
+<?php
+
+if (!defined('DIR_CORE')) {
+    header('Location: static_pages/');
+}
+
+$controllers = array(
+    'storefront' => array(
+        'pages/extension/default_pp_standart',
+        'responses/extension/simplepay_payments'
+    ),
+    'admin' => array( ),
+);
+
+$models = array(
+    'storefront' => array( 'extension/simplepay_payments' ),
+    'admin' => array( ),
+);
+
+$languages = array(
+    'storefront' => array(
+        'simplepay_payments/simplepay_payments'),
+    'admin' => array(
+        'simplepay_payments/simplepay_payments'));
+
+$templates = array(
+    'storefront' => array(
+        'responses/simplepay_payments.tpl'),
+    'admin' => array());
